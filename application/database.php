6c5e591583ee2f748f81c9cf8531c40c6634151a
weifei528 <weifei528@163.com>
2018-11-12 16:29:47 +0800

    数据库修改

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '39.105.174.115',
     // 数据库名
     'database'        => 'book',
     // 用户名
-    'username'        => 'root',
+    'username'        => 'book',
     // 密码
-    'password'        => 'root',
+    'password'        => '7TZ4CyJbT5wRCJzj',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
